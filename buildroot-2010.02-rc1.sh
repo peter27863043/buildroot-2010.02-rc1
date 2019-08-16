20d12122a5914760542e732658feaef0ca6d9616
peter27863043 <peter27863043@yahoo.com>
2019-08-16 08:45:49 +0000

    5th commit

--- buildroot-2010.02-rc1.sh
+++ buildroot-2010.02-rc1.sh
@@ -2,7 +2,7 @@ apt-get update
 apt-get install -y vim
 apt-get install -y wget
 apt-get install -y curl
-apt-get install bzip2
+apt-get install -y bzip2
 wget https://buildroot.org/downloads/buildroot-2010.02-rc1.tar.bz2
 bunzip2 buildroot-2010.02-rc1.tar.bz2
 tar -xvf buildroot-2010.02-rc1.tar
@@ -24,8 +24,9 @@ cd ..
 cd buildroot-2010.02-rc1
 mkdir dl
 cd dl
+wget http://ftp.gwdg.de/pub/linux/sources.redhat.com/binutils/releases/binutils-2.20.tar.bz2
 wget https://www.nektar.info/thirdparty/zlib-1.2.3.tar.bz2
-wget download http://sources.buildroot.net/module-init-tools-3.2.2.tar.bz2
+wget http://sources.buildroot.net/module-init-tools-3.2.2.tar.bz2
 wget https://launchpad.net/genext2fs/main/1.4/+download/genext2fs-1.4.tar.gz
 wget http://repository.timesys.com/buildsources/f/fakeroot/fakeroot-1.9.5/fakeroot-1.9.5.tar.gz
 cp fakeroot-1.9.5.tar.gz fakeroot_1.9.5.tar.gz
